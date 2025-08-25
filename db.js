e0af0af9097db5a5a841ac861030774bee1716fb
Diwei Chen <porter@Diweis-MacBook-Pro.local>
2025-08-25 17:44:43 -0400

    Fix: switch to pg.Pool instead of pg.Client

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
 // db.js
-const { Client } = require('pg');
+const { Pool } = require('pg');
 require('dotenv').config();
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
-client.connect()
-    .then(() => console.log('Connected to DB'))
-    .catch(err => console.error('DB connection error', err));
-
-module.exports = client;
+module.exports = pool;
